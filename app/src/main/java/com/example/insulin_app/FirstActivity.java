9b9ea2f48ef219bca6b2d907671c9483d457ea61
Vasyl <somikkvn@gmail.com>
2023-03-21 16:51:04 +0200

    add LoginActivity

--- app/src/main/java/com/example/insulin_app/FirstActivity.java
+++ app/src/main/java/com/example/insulin_app/FirstActivity.java
@@ -1,14 +1,51 @@
 package com.example.insulin_app;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.insulin_app.ui.login.LoginActivity;
 
 public class FirstActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInfo();
+            }
+        });
     }
+
+    public void startLoginActivity(View v){
+//        Button button6 = findViewById(R.id.button6);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+    private void showInfo(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(FirstActivity.this);
+        builder.setMessage("Реєстрація");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
